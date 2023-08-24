0ea24b4f0b9e250333570e12440d6734285b485b
Vanesa Suarez <vanesuarez2301@gmail.com>
2023-08-24 18:56:47 -0300

    items update

---
+++ js/almacenar.js
@@ -0,0 +1,43 @@
+const botonAgregar = document.getElementById("agregar");
+const botonLimpiar = document.getElementById("limpiar");
+const contenedor = document.getElementById('contenedor');
+
+// Función para cargar la lista desde el almacenamiento local al cargar la página
+function cargarLista() {
+    const listaGuardada = localStorage.getItem('lista');
+    contenedor.innerHTML = listaGuardada;
+  }
+
+  // Función para agregar un nuevo ítem a la lista
+function agregarItem() {
+    const itemInput = document.getElementById('item');
+    const nuevoItem = itemInput.value.trim();
+    if (nuevoItem !== '') {
+      const newItemElement = document.createElement('li');
+      newItemElement.className = 'list-group-item';
+      newItemElement.textContent = nuevoItem;
+      contenedor.appendChild(newItemElement);
+      // Guardar la lista en el almacenamiento local
+      const listaGuardada = contenedor.innerHTML;
+      localStorage.setItem('lista', listaGuardada);
+      // Limpiar el campo de entrada
+      itemInput.value = '';
+    }
+  }
+
+  function limpiarLista() {
+    contenedor.innerHTML = '';
+    // Limpiar la lista del almacenamiento local
+    localStorage.removeItem('lista');
+  }
+
+  
+
+// Cargar la lista al cargar la página
+cargarLista();
+// Agregar un evento al botón Agregar
+botonAgregar.addEventListener('click', agregarItem);
+// Agregar un evento al botón Limpiar
+botonLimpiar.addEventListener('click', limpiarLista);
+
+
